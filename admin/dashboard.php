f0bc1b86fb198836f3fb0bcc1b88eaa527ab7ebc
JasonGoh1210 <jason.goh.zong@student.mmu.edu.my>
2026-04-27 22:08:06 +0800

    admin

---
+++ admin/dashboard.php
@@ -0,0 +1,45 @@
+<?php
+include('../config/db.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DriveX Motors Dashboard</title>
+
+    <link rel="stylesheet" href="../css/admin_style.css">
+</head>
+
+<body>
+
+<div class="topbar">
+    <div class="logo">
+        <img src="../image/logo.png" alt="logo">
+        <h2>DriveX Motors</h2>
+    </div>
+
+    <div>
+        <a href="logout.php" ; text-decoration:none;">Logout</a>
+    </div>
+</div>
+
+<div class="sidebar">
+    <a href="dashboard.php">Overview</a>
+    <a href="manage_car.php">Manage Car</a>
+    <a href="manage_booking.php">Manage Booking</a>
+    <a href="manage_enquiry.php">Manage Enquiry</a>
+    <a href="../index.php">View Website</a>
+</div>
+
+<div class="content">
+
+    <div class="card">
+        <h3>Dashboard</h3>
+        <p>Welcome to DriveX Motors Admin Page.</p>
+    </div>
+
+</div>
+
+</body>
+</html>
