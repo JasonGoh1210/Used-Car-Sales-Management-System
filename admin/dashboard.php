da6e3d9fbfda357712e1a2e674e16a9fb7ce2d2e
JasonGoh1210 <jason.goh.zong@student.mmu.edu.my>
2026-04-27 20:58:54 +0800

    admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,39 +1,53 @@
+<?php
+session_start();
+include("../config/db.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>DriveX Motors Dashboard</title>
-
     <link rel="stylesheet" href="../css/admin_style.css">
 </head>
 
 <body>
 
-<div class="topbar">
-    <div class="logo">
-        <img src="../image/logo.png" alt="logo">
-        <h2>DriveX Motors</h2>
-    </div>
-
-    <div>
-        <a href="logout.php" ; text-decoration:none;">Logout</a>
-    </div>
-</div>
-
 <div class="sidebar">
-    <a href="dashboard.php">Overview</a>
+
+    <div class="sidebar-header">
+        <img src="../image/logo.png">
+        <h3>DriveX Motors</h3>
+    </div>
+
+    <p class="menu-title">MENU</p>
+
+    <a href="dashboard.php" class="active">Overview</a>
     <a href="manage_car.php">Manage Car</a>
-    <a href="manage_booking.php">Manage Booking</a>
-    <a href="manage_enquiry.php">Manage Enquiry</a>
+    <a href="manage_booking.php">Bookings</a>
+    <a href="manage_enquiry.php">Enquiry</a>
     <a href="../index.php">View Website</a>
+
+    <div class="sidebar-bottom">
+        <p class="signed">SIGNED AS</p>
+        <p class="email">
+            <?php 
+            echo isset($_SESSION['admin_username']) 
+                ? $_SESSION['admin_username'] 
+                : 'admin@gmail.com'; 
+            ?>
+        </p>
+
+        <a href="logout.php" class="logout">Logout</a>
+    </div>
+
 </div>
 
 <div class="content">
-
     <div class="card">
         <h3>Dashboard</h3>
-        <p>Welcome to DriveX Motors Admin Page.</p>
+        <p>Welcome Back.</p>
     </div>
-
 </div>
 
 </body>
